3a4d92c39570fc00cab8ad16628f6b83aec2aac6
Bobby Upreti <bobbyupreti1234@gmail.com>
2023-12-12 01:01:11 +0530

    cyclic uploaded

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://greenglobalaggrovation.onrender.com";
+// export const BASE_URL = "https://greenglobalaggrovation.onrender.com";
+export const BASE_URL = "https://eager-toad-sunbonnet.cyclic.app";
