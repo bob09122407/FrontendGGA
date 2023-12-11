8e8f9f33435a0437ba7cdf3abe0bb3d75c3f8853
Bobby Upreti <bobbyupreti1234@gmail.com>
2023-12-11 21:38:09 +0530

    stripe added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -18,13 +18,17 @@ import ResetPassword from './component/User/ResetPassword';
 import Cart from './component/Cart/Cart';
 import Shipping from './component/Cart/Shipping';
 import ConfirmOrder from './component/Cart/ConfirmOrder';
-import Payment from './component/Cart/Payment';
+import Payment from './component/Cart/Stripepayment';
 import OrderSuccess from './component/Cart/OrderSuccess';
 import MyOrders from './component/Order/MyOrders';
 import OrderDetails from './component/Order/OrderDetails';
 import ContactPage from './component/Contact/contact';
+import { loadStripe } from '@stripe/stripe-js';
 // import About from './component/About/about';
 import axios from 'axios';
+import { loadUser } from './actions/userAction';
+import {Elements} from "@stripe/react-stripe-js";
+import store from "./store";
 
 function ProtectedRoute({ isAdmin, component: Component, ...rest }) {
   const { loading, isAuthenticated, user } = useSelector((state) => state.user);
@@ -47,19 +51,38 @@ function ProtectedRoute({ isAdmin, component: Component, ...rest }) {
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
 
-  useEffect(() => {
-    // Load the Razorpay script dynamically
-    const loadRazorpayScript = async () => {
-      const script = document.createElement('script');
-      script.src = 'https://checkout.razorpay.com/v1/checkout.js';
-      script.async = true;
-      script.onload = () => {
-        // Razorpay script is loaded, you can initialize it here if needed
-      };
-      document.body.appendChild(script);
-    };
+  const [stripeApiKey, setStripeApiKey]= useState("");
 
-    loadRazorpayScript();
+  async function getStripeApiKey(){
+    const {data} =await axios.get("/api/v1/stripeapikey");
+
+    setStripeApiKey(data.stripeApiKey);
+  }
+  // const options = {
+  //   mode: 'payment',
+  //   currency: 'inr',
+  //   amount: 1099,
+  // };
+
+  // useEffect(() => {
+  //   // Load the Razorpay script dynamically
+  //   const loadRazorpayScript = async () => {
+  //     const script = document.createElement('script');
+  //     script.src = 'https://checkout.razorpay.com/v1/checkout.js';
+  //     script.async = true;
+  //     script.onload = () => {
+  //       // Razorpay script is loaded, you can initialize it here if needed
+  //     };
+  //     document.body.appendChild(script);
+  //   };
+
+  //   loadRazorpayScript();
+  // }, []);
+
+  useEffect(()=>{
+    store.dispatch(loadUser());
+
+    getStripeApiKey();
   }, []);
 
   return (
@@ -86,7 +109,17 @@ function App() {
         <Route exact path="/success" element={<ProtectedRoute component={OrderSuccess} />} />
         <Route exact path="/orders" element={<ProtectedRoute component={MyOrders} />} />
         <Route exact path="/order/:id" element={<ProtectedRoute component={OrderDetails} />} />
-        <Route exact path="/process/payment" element={<ProtectedRoute component={Payment} />} />
+        {/* <Route exact path="/process/payment" element={<ProtectedRoute component={Payment} />} /> */}
+        <Route
+          exact
+          path="/process/payment"
+          element={
+            
+            <Elements stripe={loadStripe(stripeApiKey)}>
+              <ProtectedRoute component={Payment} />
+            </Elements>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
---
+++ src/component/Cart/Stripepayment.js
@@ -0,0 +1,186 @@
+import React, { Fragment, useEffect, useRef } from "react";
+import CheckoutSteps from "../Cart/CheckoutSteps";
+import { useSelector, useDispatch } from "react-redux";
+import MetaData from "../layout/MetaData";
+import { Typography } from "@material-ui/core";
+import { useAlert } from "react-alert";
+import {
+  CardNumberElement,
+  CardElement,
+  PaymentElement,
+  CardCvcElement,
+  CardExpiryElement,
+  ExpressCheckoutElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+import axios from "axios";
+import "./stripepayment.css";
+import CreditCardIcon from "@material-ui/icons/CreditCard";
+import EventIcon from "@material-ui/icons/Event";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import {createOrder, clearErrors} from "../../actions/orderAction";
+import { clearCart } from "../../actions/cartAction";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "./../../apiConfig";
+const Payment = () => {
+  const orderInfo = JSON.parse(sessionStorage.getItem("orderInfo"));
+
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const stripe = useStripe();
+  const elements = useElements();
+  const payBtn = useRef(null);
+ const navigate=useNavigate();
+  const { shippingInfo, cartItems } = useSelector((state) => state.cart);
+  const { user } = useSelector((state) => state.user);
+  const { error } = useSelector((state) => state.newOrder);
+  console.log(<ExpressCheckoutElement/>);
+  const paymentData = {
+    amount: Math.round(orderInfo.totalPrice * 100),
+  };
+
+  const order = {
+    shippingInfo,
+    orderItems: cartItems,
+    itemsPrice: orderInfo.subtotal,
+    taxPrice: orderInfo.tax,
+    shippingPrice: orderInfo.shippingCharges,
+    totalPrice: orderInfo.totalPrice,
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    payBtn.current.disabled = true;
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        `${BASE_URL}/api/v1/payment/process`,
+        paymentData,
+        config
+      );
+
+      const client_secret = data.client_secret;
+
+      if (!stripe || !elements) return;
+
+
+
+      const result = await stripe.confirmCardPayment(client_secret, {
+        payment_method: {
+          card: elements.getElement(CardNumberElement),
+          billing_details: {
+            name: user.name,
+            email: user.email,
+            address: {
+              line1: shippingInfo.address,
+              city: shippingInfo.city,
+              state: shippingInfo.state,
+              postal_code: shippingInfo.pinCode,
+              country: shippingInfo.country,
+            },
+          },
+        },
+      });
+    
+      if (result.error) {
+        payBtn.current.disabled = false;
+
+        alert.error(result.error.message);
+      } else {
+        if (result.paymentIntent.status === "succeeded") {
+          order.paymentInfo = {
+            id: result.paymentIntent.id,
+            status: result.paymentIntent.status,
+          };
+
+          dispatch(createOrder(order));
+
+          navigate("/success");
+          dispatch(clearCart());
+        } else {
+          alert.error("There's some issue while processing payment ");
+        }
+      }
+    } catch (error) {
+      payBtn.current.disabled = false;
+      // alert.error(error.response.data.message);
+      alert.error(error.response?.data?.message || "An error occurred while processing payment");
+    }
+
+    
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+  }, [dispatch, error, alert]);
+
+  return (
+    <Fragment>
+      <MetaData title="Payment" />
+      <CheckoutSteps activeStep={2} />
+      <div className="paymentContainer">
+        {/* <form className="paymentForm" onSubmit={submitHandler}>
+         
+    <PaymentElement />
+    <input
+            type="submit"
+            value={`Pay - ₹${orderInfo && orderInfo.totalPrice}`}
+            ref={payBtn}
+            className="paymentFormBtn"
+          />  */}
+
+        {/* </form> */}
+{/* 
+          <div id="checkout-page">
+            <CardElement/>
+            <input
+            type="submit"
+            value={`Pay - ₹${orderInfo && orderInfo.totalPrice}`}
+            ref={payBtn}
+            className="paymentFormBtn" onClick={submitHandler}
+          />  
+      <ExpressCheckoutElement onConfirm={submitHandler} />
+    </div> */}
+       <form className="paymentForm" onSubmit={submitHandler}>
+          <Typography>Card Info</Typography>
+          <div>
+            <CreditCardIcon />
+            <CardNumberElement options={{ style: { base: { fontSize: '16px', color: '#fff'} } }} className="paymentInput" />
+          </div>
+          <div>
+            <EventIcon />
+            <CardExpiryElement options={{ style: { base: { fontSize: '16px', color: '#fff' } } }} className="paymentInput" />
+          </div>
+          <div>
+            <VpnKeyIcon />
+            <CardCvcElement options={{ style: { base: { fontSize: '16px' , color: '#fff'} } }} className="paymentInput" />
+          </div>
+
+          <input
+            type="submit"
+            value={`Pay - ₹${orderInfo && orderInfo.totalPrice}`}
+            ref={payBtn}
+            className="paymentFormBtn"
+          />
+          <ExpressCheckoutElement onConfirm={submitHandler} />
+        </form>
+        
+      </div>
+    </Fragment>
+  );
+};
+
+export default Payment;
+
+
