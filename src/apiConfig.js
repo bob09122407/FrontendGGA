d4a49072c093b1ef1b00188a0f6bfeb919c23d2d
Bobby Upreti <bobbyupreti1234@gmail.com>
2024-05-09 15:38:20 +0530

    backend url change

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "https://greenglobalaggrovation.onrender.com";
-export const BASE_URL = "https://sparkling-slug-flannel-shirt.cyclic.app";
+// export const BASE_URL = "https://sparkling-slug-flannel-shirt.cyclic.app";
+export const BASE_URL = "http://backendggaapp.ap-south-1.elasticbeanstalk.com/";
